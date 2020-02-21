25485951155819078031239c2dc33b0ce764e8b3
X-Anas-X <anas.zain1996@gmail.com>
2020-02-21 12:52:18 +0200

    q2 answer

--- app.js
+++ app.js
@@ -29,11 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var multi = a*b;
+    var msg = `The product of ${a} and ${b} is ${multi}.`;
+    return[multi, msg];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,7 +52,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
